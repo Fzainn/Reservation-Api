2ffa875b82a288060736110bfc7d72a0459f5200
fatha <fzainn99@gmail.com>
2024-03-22 16:02:29 +0200

    belong_to explination

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ApplicationRecord
 
-	#Association
+	#Each reservation is associated with a specific venue and specific guest
+	#this implies in db schema, in 'reservation' table there are a columm called 'venue_id' and 'guest_id'
+	#which store the id for this specific venue and id, that represents that reservation belongs to this venue and this guest
 	belongs_to :venue
 	belongs_to :guest
 
